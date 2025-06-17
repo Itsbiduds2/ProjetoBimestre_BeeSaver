aac0ee7bc9723baa4553e705d8a446b7fa83c581
lmv <martins.valentim@aluno.ifsp.edu.br>
2025-06-17 15:50:52 -0300

    mari milk2

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -3,7 +3,9 @@ import style from "./Button.module.css"
 function Button({texto, tipo}) {
     return (
       <>
-        <button className={tipo=="link" ? style.link : style.publicar  }>
+        <button
+          className={tipo === "link" ? style.link : tipo === "categoria" ? style.categoria : style.publicar}
+        >
             {texto}
         </button>
       </>
