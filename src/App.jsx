d78c0588d5d816e7d0822dbf0ac1344ad81084c6
lmv <martins.valentim@aluno.ifsp.edu.br>
2025-06-20 14:46:02 -0300

    mari79

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,9 @@ import { useState } from 'react';
 
 function App() {
 
-  const [estadoSelecionado, setEstadoSelecionado] = useState('');
-  const estados = [
-    'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA',
-    'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN',
-    'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'
-  ];
-  
-
   return (
     <>
-      <Button texto={"Orquideas"} tipo={"categoria"}/>      
-      
-      <br />
-
-      <Select
-        label="Estado"
-        options={estados}
-        value={estadoSelecionado}
-        onChange={setEstadoSelecionado}
-      />
+     
     </>
   )
 }
