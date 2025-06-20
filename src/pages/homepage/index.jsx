df05cc91916b013d50baa58958b05296029a6874
lmv <martins.valentim@aluno.ifsp.edu.br>
2025-06-20 15:51:14 -0300

    marimilk82

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,3 +1,4 @@
+import Button from "../../components/Button"
 import Header from "../../components/header/Header"
 import style from "./homepage.module.css"
 
@@ -5,6 +6,34 @@ function Homepage() {
     return (
       <>
       <Header/>
+      <main className={style.main}>
+        <h2 className={style.title}>Inicio</h2>
+        <hr className={style.line}/>
+
+        <div className={style.buttons}>
+            <Button texto={"Abelhas"} tipo={"link"}/>
+            <Button texto={"Jardim"} tipo={"link"}/>
+            <Button texto={"Mapa"} tipo={"link"}/>
+        </div>
+
+        <div className={style.videoContainer}>
+          <iframe
+            width="300"
+            height="170"
+            src="https://www.youtube.com/embed/sua-url-aqui"
+            title="BeeSaver: Facilitando Esforços Comunitários para o Crescimento da População de Abelhas🐝"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+
+        <p className={style.description}>
+          Por que as abelhas são tão importantes para o nosso planeta? E por que devemos preservá-las?
+          <br />
+          Conheça o projeto inovador BeeSaver desenvolvido por alunos do Instituto Federal de Bragança Paulista
+        </p>
+
+      </main>
         
       </>
     )
---
+++ src/pages/mapa/index.jsx
@@ -0,0 +1,9 @@
+function Mapa(){ 
+    return (
+      <>
+
+      </>
+    )
+}
+    
+export default Mapa
